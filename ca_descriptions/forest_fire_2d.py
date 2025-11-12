fd8fb2b2916f2b63078d5f21d5dfaed88e309c6a
Rifat Rahman <rifatrahman2003@gmail.com>
2025-11-12 11:01:43 +0000

    added jeon's slow burning

--- ca_descriptions/forest_fire_2d.py
+++ ca_descriptions/forest_fire_2d.py
@@ -16,7 +16,7 @@ from capyle.ca import Grid2D, Neighbourhood, CAConfig, randomise2d
 import capyle.utils as utils
 import numpy as np
 
-
+# state definitions (fire + terrain)
 UNBURNT = 0
 BURNING = 1
 BURNT = 2
@@ -24,6 +24,18 @@ LAKE = 3
 CHAPARRAL = 4
 DENSE_FOREST = 5
 CANYON = 6
+
+# how many cycles a cell remain burning before burnt
+BURN_DURATION = {CHAPARRAL: 6, DENSE_FOREST: 15, CANYON: 3}  # Lake never burns
+
+# base ignition probability by terrain
+IGNITION_PROB = {CHAPARRAL: 0.70, DENSE_FOREST: 0.35, CANYON: 0.90}
+
+# thresholds for ignition, forest needs more than 2 burning neighboeru, not easy to ignite but longer burning cycle
+TERRAIN_MIN_NEIGHBOURS = {CHAPARRAL: 1, DENSE_FOREST: 2, CANYON: 1}
+
+
+# need to make a function that count burning neighbours
 
 
 def transition_func(grid, neighbourstates, neighbourcounts, decay_grid):
@@ -39,13 +51,54 @@ def transition_func(grid, neighbourstates, neighbourcounts, decay_grid):
 
     grid_copy = grid.copy()
 
-    on_fire = (burning_neighbours >= 1) & (grid == UNBURNT)
+    # random grid
+    rng = np.random.random(grid.shape)  # random in [0,1) per cell
+
+    # tick burning cells and become burnt when timer ends
+    was_burning = grid_copy == BURNING
+    if was_burning.any():
+        decay_grid[was_burning] -= 1
+        finished = was_burning & (decay_grid <= 0)
+        grid[finished] = BURNT
+        decay_grid[finished] = 0
+
+    # Terrain masks -> treat terrain values as unburnt
+    is_lake = grid_copy == LAKE
+    is_chaparral = grid_copy == CHAPARRAL
+    is_forest = grid_copy == DENSE_FOREST
+    is_canyon = grid_copy == CANYON
+
+    # Terrain-specific neighbour thresholds (use burning_neighbours from neighbourcounts)
+    # ready as ready to spread/ can spread fire
+    # ready only if burning neighbours bigger or equal to minimum neighbours of each terrain
+    chap_ready = is_chaparral & (
+        burning_neighbours >= TERRAIN_MIN_NEIGHBOURS[CHAPARRAL]
+    )
+    forest_ready = is_forest & (
+        burning_neighbours >= TERRAIN_MIN_NEIGHBOURS[DENSE_FOREST]
+    )
+    canyon_ready = is_canyon & (burning_neighbours >= TERRAIN_MIN_NEIGHBOURS[CANYON])
+
+    # Probabilistic ignition per terrain
+    # ignite only when terrain is ready to ignite and when the ignititon probability is bigger than random grid
+    ignite_chap = chap_ready & (rng < IGNITION_PROB[CHAPARRAL])
+    ignite_forest = forest_ready & (rng < IGNITION_PROB[DENSE_FOREST])
+    ignite_canyon = canyon_ready & (rng < IGNITION_PROB[CANYON])
+
+    on_fire = ignite_chap | ignite_forest | ignite_canyon
 
     # Set cells to BURNING where on_fire condition is met
     grid[on_fire] = BURNING
 
-    # Cells that are BURNING become BURNT
-    grid[grid_copy == BURNING] = BURNT
+    # initialise burn timer
+    if on_fire.any():
+        chaparral_neighbours = on_fire & is_chaparral
+        dense_forest_neighbours = on_fire & is_forest
+        canyon_neighbours = on_fire & is_canyon
+
+        decay_grid[chaparral_neighbours] = BURN_DURATION[CHAPARRAL]
+        decay_grid[dense_forest_neighbours] = BURN_DURATION[DENSE_FOREST]
+        decay_grid[canyon_neighbours] = BURN_DURATION[CANYON]
 
     return grid
 
@@ -79,7 +132,7 @@ def setup(args):
     ]
 
     config.num_generations = 150
-    config.grid_dims = (20, 20)
+    config.grid_dims = (200, 200)
     config.wrap = False
 
     if len(args) == 2:
@@ -94,7 +147,8 @@ def main():
     config = setup(sys.argv[1:])
 
     decay_grid = np.zeros(config.grid_dims)
-    decay_grid.fill(2)
+    # decay_grid.fill(2)
+    # -> lakes and unlit terrain starts with the timer
 
     # Create grid object
     grid = Grid2D(
